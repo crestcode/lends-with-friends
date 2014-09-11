60fd0d6d12b4d3d2501d7c7a6fa217167e136615
John Kasiewicz <crestcode@gmail.com>
2014-09-11 18:18:13 -0400

    create friends and return JSON responses

--- spec/controllers/friends_controller_spec.rb
+++ spec/controllers/friends_controller_spec.rb
@@ -1,22 +1,38 @@
 require 'rails_helper'
 
 describe FriendsController do
-  it 'should be able to create a new user' do
-    post :create, :friend => {first_name: 'Test', last_name: 'User', email: 'test@test.com', phone: '555-555-5555'}, format: :json
+  describe '#index' do
+    it 'should return json of array with friends list' do
+      get :index, format: :json
 
-    expect(response.status).to eq(200)
+      expect(response.status).to eq(200)
 
-    json_response = JSON.parse(response.body)
-    expect(json_response['first_name']).to eql('Test')
-    expect(json_response['last_name']).to eql('User')
-    expect(json_response['email']).to eql('test@test.com')
-    expect(json_response['phone']).to eql('555-555-5555')
+      json_response = JSON.parse(response.body).first
+      expect(json_response['first_name']).to eql(user.first_name)
+      expect(json_response['last_name']).to eql(user.last_name)
+      expect(json_response['email']).to eql(user.email)
+      expect(json_response['phone']).to eql(user.phone)
+    end
   end
 
-  it 'should respond with 204 status and error messages if invalid friend data' do
-    post :create, :friend => {first_name: nil, last_name: 'Tester', email: 'test@test.com'}, format: :json
+  describe '#create' do
+    it 'should be able to create a new user' do
+      post :create, :friend => {first_name: 'Test', last_name: 'User', email: 'test@test.com', phone: '555-555-5555'}, format: :json
 
-    expect(response.status).to eq(204)
-    expect(JSON.parse(response.body)).to eql('friend' => {'first_name' => ['can\'t be blank']})
+      expect(response.status).to eq(200)
+
+      json_response = JSON.parse(response.body)
+      expect(json_response['first_name']).to eql('Test')
+      expect(json_response['last_name']).to eql('User')
+      expect(json_response['email']).to eql('test@test.com')
+      expect(json_response['phone']).to eql('555-555-5555')
+    end
+
+    it 'should respond with 204 status and error messages if invalid friend data' do
+      post :create, :friend => {first_name: nil, last_name: 'Tester', email: 'test@test.com'}, format: :json
+
+      expect(response.status).to eq(204)
+      expect(JSON.parse(response.body)).to eql('friend' => {'first_name' => ['can\'t be blank']})
+    end
   end
 end
